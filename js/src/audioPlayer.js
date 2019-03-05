e56c277f5d78318dc440a15008e19b98317dabd2
HHHHHG\宏宏宏宏宏哥 <2894700792@qq.com>
2019-03-05 13:15:51 +0800

    Site updated: 2019-03-05 13:15:49

--- js/src/audioPlayer.js
+++ js/src/audioPlayer.js
@@ -9,30 +9,40 @@
 // });
 
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('aplayer'),
     mini: true,
     autoplay: true,
     theme: '#FADFA3',
     loop: 'all',
-    order: 'list',
+    order: 'random',
     preload: 'auto',
     volume: 0.7,
     mutex: true,
     fixed: true,
     audio: [
         {
-            name: 'name1',
+            name: 'この感情は使命',
             artist: '安瀬聖',
             url: 'http://music.163.com/song/media/outer/url?id=412951840.mp3',
-            cover: 'cover1.jpg',
-            theme: '#ebd0c2'
+            cover: 'https://pic.ggemo.com/picgo/19200771556072853.jpg',
         },
         {
             name: 'ちいさな冒険者',
             artist: '茅野爱衣/雨宫天',
             url: 'http://music.163.com/song/media/outer/url?id=41645488.mp3',
-            cover: 'cover2.jpg',
-            theme: '#46718b'
+            cover: 'https://pic.ggemo.com/picgo/3386495816508708.jpg',
+        },
+        {
+            name: "Arrietty's Song",
+            artist: 'Cecile Corbel',
+            url: 'http://music.163.com/song/media/outer/url?id=2324487.mp3',
+            cover: 'https://pic.ggemo.com/picgo/1716337650959823.jpg',
+        },
+        {
+            name: "さよならの夏",
+            artist: '手嶌葵',
+            url: 'http://music.163.com/song/media/outer/url?id=656405.mp3',
+            cover: 'https://pic.ggemo.com/picgo/2267192976481272.jpg',
         }
     ]
 });

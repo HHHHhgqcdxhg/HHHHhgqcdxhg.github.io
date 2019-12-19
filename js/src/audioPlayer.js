fe597c06ec076aa26cb5ff994a791a823774aa05
HHHHHG <2894700792@qq.com>
2019-12-19 15:01:56 +0800

    Site updated: 2019-12-19 15:01:52

--- js/src/audioPlayer.js
+++ js/src/audioPlayer.js
@@ -20,47 +20,47 @@ const ap = new APlayer({
     mutex: true,
     fixed: true,
     audio: [
-        {
-            name: 'この感情は使命',
-            artist: '安瀬聖',
-            url: 'https://music.163.com/song/media/outer/url?id=412951840.mp3',
-            cover: 'https://pic.ggemo.com/picgo/19200771556072853.jpg',
-        },
-        {
-            name: 'ちいさな冒険者',
-            artist: '茅野爱衣/雨宫天',
-            url: 'https://music.163.com/song/media/outer/url?id=41645488.mp3',
-            cover: 'https://pic.ggemo.com/picgo/3386495816508708.jpg',
-        },
-        {
-            name: "Arrietty's Song",
-            artist: 'Cecile Corbel',
-            url: 'https://music.163.com/song/media/outer/url?id=2324487.mp3',
-            cover: 'https://pic.ggemo.com/picgo/1716337650959823.jpg',
-        },
-        {
-            name: "さよならの夏",
-            artist: '手嶌葵',
-            url: 'https://music.163.com/song/media/outer/url?id=656405.mp3',
-            cover: 'https://pic.ggemo.com/picgo/2267192976481272.jpg',
-        },
+        // {
+        //     name: 'この感情は使命',
+        //     artist: '安瀬聖',
+        //     url: 'https://music.163.com/song/media/outer/url?id=412951840.mp3',
+        //     cover: 'https://pic.ggemo.com/picgo/19200771556072853.jpg',
+        // },
+        // {
+        //     name: 'ちいさな冒険者',
+        //     artist: '茅野爱衣/雨宫天',
+        //     url: 'https://music.163.com/song/media/outer/url?id=41645488.mp3',
+        //     cover: 'https://pic.ggemo.com/picgo/3386495816508708.jpg',
+        // },
+        // {
+        //     name: "Arrietty's Song",
+        //     artist: 'Cecile Corbel',
+        //     url: 'https://music.163.com/song/media/outer/url?id=2324487.mp3',
+        //     cover: 'https://pic.ggemo.com/picgo/1716337650959823.jpg',
+        // },
+        // {
+        //     name: "さよならの夏",
+        //     artist: '手嶌葵',
+        //     url: 'https://music.163.com/song/media/outer/url?id=656405.mp3',
+        //     cover: 'https://pic.ggemo.com/picgo/2267192976481272.jpg',
+        // },
         {
             name: "urar",
             artist: 'Chima',
-            url: 'https://music.163.com/song/media/outer/url?id=532776144.mp3',
-            cover: 'https://pic.ggemo.com/picgo/109951163117275948.jpg',
+            url: 'https://pic.ggemo.com/urar%20-%20Chima.mp3',
+            cover: 'https://pic.ggemo.com/T002R300x300M000001k7VKU28Phst.jpg',
         },
-        {
-            name: "Growing!",
-            artist: '佐藤しずく',
-            url: 'https://music.163.com/song/media/outer/url?id=653660.mp3',
-            cover: 'https://pic.ggemo.com/picgo/5905476952948165.jpg',
-        },
+        // {
+        //     name: "Growing!",
+        //     artist: '佐藤しずく',
+        //     url: 'https://music.163.com/song/media/outer/url?id=653660.mp3',
+        //     cover: 'https://pic.ggemo.com/picgo/5905476952948165.jpg',
+        // },
         {
             name: "春よ、来い",
-            artist: '松任谷由実',
-            url: 'https://music.163.com/song/media/outer/url?id=615891.mp3',
-            cover: 'https://pic.ggemo.com/picgo/20190307084435.png',
+            artist: 'Smooth J',
+            url: 'https://music.163.com/song/media/outer/url?id=511369.mp3',
+            cover: 'https://pic.ggemo.com/6050612487861686.jpg',
         }
     ]
 });
